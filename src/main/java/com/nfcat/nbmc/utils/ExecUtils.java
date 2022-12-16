3c81bb2d665b13201a19855f4cd5e1308889d516
mcmy <775720345@qq.com>
2022-12-16 17:53:52 +0800

    增加mc原版命令（screen）

--- src/main/java/com/nfcat/nbmc/utils/ExecUtils.java
+++ src/main/java/com/nfcat/nbmc/utils/ExecUtils.java
@@ -1,18 +1,29 @@
 package com.nfcat.nbmc.utils;
 
+import com.nfcat.nbmc.NbMcPlugin;
 import com.nfcat.nbmc.data.ExecCallback;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class ExecUtils {
+
+    public static boolean execMcHttpCommand(String command) {
+        return false;
+    }
+
+    public static boolean execMcShellCommand(String command) {
+        ExecCallback exec = exec(Charset.defaultCharset().name(), NbMcPlugin.getPropConfig("default_command.execute.command"), command);
+        return exec.getExitValue() == 0;
+    }
 
     public static @NotNull ExecCallback exec(String charset, String command, Object... format) {
         ExecCallback callback = new ExecCallback();
         try {
             Process process = Runtime.getRuntime().exec(String.format(command, format));
-            if (charset == null) charset = "gbk";
+            if (charset == null) charset = "UTF-8";
             callback.setCallback(new String(new BufferedInputStream(process.getInputStream()).readAllBytes(), charset));
             callback.setCallBackError(new String(new BufferedInputStream(process.getErrorStream()).readAllBytes(), charset));
             callback.setExitValue(process.exitValue());
